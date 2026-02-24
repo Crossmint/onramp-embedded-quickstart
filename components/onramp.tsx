a736c71b796022e00699397f1b8be4c9e86d6bc1
jcurbelo <robin@paella.dev>
2026-02-24 12:05:35 -0500

    fixes syntax error

--- components/onramp.tsx
+++ components/onramp.tsx
@@ -123,6 +123,7 @@ export default function Onramp() {
                     setUserType("returning");
                     setReceiptEmail("demos+onramp-existing-user@crossmint.com");
                     setRecipientWalletAddress(RETURNING_USER_RECIPIENT_WALLET);
+                  }}
                 />
               )}
             </div>
